601e0ec07eccb030c80cba5d66e61e3fe849fb14
hyoming1 <128107630+hyoming1@users.noreply.github.com>
2025-09-01 03:45:26 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -45,26 +45,29 @@ else
     # 5. Cloud-init 초기화
     cloud-init clean --logs
     rm -rf /var/lib/cloud/*
-    
-    # 6. root 계정 비활성화
-    passwd -l root
      
-    # 7. 저널 로그 전체 삭제 (완전 초기화)
+    # 6. 저널 로그 전체 삭제 (완전 초기화)
     rm -rf /var/log/journal/*
 
-    # 8. 임시파일 정리
+    # 7. 임시파일 정리
     rm -rf /tmp/* /var/tmp/*
+
+    # 8. 네트워크 정보 삭제
+    rm -f /etc/NetworkManager/system-connections/*
+
+    # 9. root 계정 비활성화
+    passwd -l root
     
-    # 9. 셸 히스토리 초기화
+    # 10. 셸 히스토리 초기화
     cat /dev/null > ~/.bash_history
     history -c
 
-    # 10. 재부팅 플래그 제거
+    # 11. 재부팅 플래그 제거
     rm -f $FLAG
 
-    # 11. 스크립트 자체 삭제
+    # 12. 스크립트 자체 삭제
     rm -- "$0"
 
-    # 12. 시스템 종료
+    # 13. 시스템 종료
     poweroff
 fi
